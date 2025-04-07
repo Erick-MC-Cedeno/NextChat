3e63cb788cc46aa08862f418cc71452191389724
Cisco101010 <erickcedeno.ec@gmail.com>
2025-04-07 11:07:54 -0600

    New Updates

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors"; // Importar cors
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -17,6 +18,7 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:3000" })); 
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
@@ -25,10 +27,10 @@ app.use("/api/users", userRoutes);
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 server.listen(PORT, () => {
-	connectToMongoDB();
-	console.log(`Server Running on port ${PORT}`);
+    connectToMongoDB();
+    console.log(`Server Running on port ${PORT}`);
 });
